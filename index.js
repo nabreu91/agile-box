9fdd89621b19bdbab37c6be2f8f1699249640d21
Matias Olivera <moliverafreire@gmail.com>
2015-10-29 17:51:40 -0300

    Add comments endpoints

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('node-uuid');
 const app = express();
+
+const comments = [];
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/api/comments', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache');
+  res.json(comments);
+});
+
+app.post('/api/comments', (req, res) => {
+  const comment = req.body;
+  comment.id = uuid.v4();
+  comments.push(comment);
+
+  res.setHeader('Cache-Control', 'no-cache');
+  res.json(comment);
 });
 
 if (!module.parent) {
