f2ca5649a995c123e41784371c5cb5aad512ae3b
Matias Olivera <moliverafreire@gmail.com>
2015-10-29 20:11:08 -0300

    Add comment list and form

---
+++ test/functional/comments-box.js
@@ -0,0 +1,30 @@
+const Browser = require('zombie');
+const app = require('../../');
+
+describe('User visits home page', () => {
+
+  const browser = new Browser({ site: 'http://127.0.0.1:3000' });
+
+  before((done) => {
+    app.listen(3000, () => {
+      browser.visit('/', done);
+    });
+  });
+
+  it('should see the home page', () => {
+    browser.assert.text('title', 'Agile Box');
+  });
+
+  describe('posts comment', () => {
+    before((done) => {
+      browser
+        .fill('author', 'Me')
+        .fill('body', 'Hey, yo!')
+        .pressButton('Post', done);
+    });
+
+    it('should see the comment', function() {
+      browser.assert.text('.comment', 'Hey, yo!');
+    });
+  });
+});
